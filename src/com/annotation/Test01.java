9d8fec46809c2915e5e741ed10a81238ab7d822c
zj <415559852@qq.com>
2021-07-25 19:21:17 +0800

    内置注解

---
+++ src/com/annotation/Test01.java
@@ -0,0 +1,30 @@
+package com.annotation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test01 {
+
+    //Override 重写的注解
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    //Deprecated 不推荐程序员使用,但是可以使用,或者存在更好的方式
+    @Deprecated
+    public static void test() {
+        System.out.println("Deprecated");
+    }
+
+    @SuppressWarnings("all")
+    public void test02() {
+        List list = new ArrayList();
+    }
+
+    public static void main(String[] args) {
+        test();
+    }
+
+
+}
